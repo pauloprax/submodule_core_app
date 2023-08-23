9ab5c8150dfd71c63620cc67e07dedd79677a21c
paulo <paulo@monetizemore.com>
2023-08-23 20:42:50 -0300

    updating templates

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+templates = Jinja2Templates(directory="local_templates/")
 # app.mount("/static", StaticFiles(directory="static"), name="static")
-# templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/testing_template", response_class=HTMLResponse)
+async def testing_template(request: Request):
+    return templates.TemplateResponse("test.html",{"request": request})
